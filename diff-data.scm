56024afab4a8344c43a97775f833ffc39146e060
Jacob Reckhard <jacobreckhard@gmail.com>
2021-04-20 10:04:11 -0600

    added diff from master

--- diff-data.scm
+++ diff-data.scm
@@ -17,7 +17,7 @@
 (define (process-diff-on-master diff master)
   (cond
     ((diff-add? diff) (cons (event-remove-note! diff "jdiff") master))
-    ((diff-remove? diff) (filter (lambda (y) (not (equal? y (event-remove-note! diff "jdiff")))) master))
+    ((diff-remove? diff) (filter (lambda (y) (not (event=? y diff))) master))
     (else (println "Error item not a diff") (values))))
 (define (jlife-data)
   (define master (load-data))
@@ -43,3 +43,29 @@
 (define (diff-reset-master)
   (save-data (jlife-data))
   (save-diff '()))
+
+(define (generate-diffs-from-masters m1 m2)
+  (define ignore (lset-intersection event=? m1 m2))
+  (define added (lset-difference event=? m2 ignore))
+  (define removed (lset-difference event=? m1 ignore))
+  (append (map (lambda (x) (event-add-note! x '("jdiff" . "add"))) added)
+          (map (lambda (x) (event-add-note! x '("jdiff" . "sub"))) removed)))
+
+#|;; generate-diffs-from-masters test
+(use-modules (ice-9 copy-tree))
+(println
+  (equal?
+    '((todo "test4" #f (("jdiff" . "add") ("done" . #f)))
+      (todo "test1" #f (("jdiff" . "sub") ("done" . #f))))
+    (generate-diffs-from-masters
+      (copy-tree
+        '((todo "test1" #f (("done" . #f)))
+          (todo "test2" #f (("done" . #f)))
+          (todo "test3" #f (("done" . #f)))))
+      (copy-tree
+        '((todo "test4" #f (("done" . #f)))
+          (todo "test2" #f (("done" . #f)))
+          (todo "test3" #f (("done" . #f))))))))
+;;|#
+
+
